eb7b068829e7efa62be57b22e612f0d288a1ba34
Torben <59419684+entorb@users.noreply.github.com>
2020-10-17 23:37:19 +0200

    added fritzbox hosts fetching

---
+++ fritzhosts.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+# reads list of active hosts from Fritzbox
+
+# installation
+# pip3 install fritzconnection
+
+from InfluxUploader import InfluxUploader
+from fritzconnection.lib.fritzhosts import FritzHosts
+from fritzconnection import FritzConnection
+
+influx = InfluxUploader(verbose=False)
+my_measurement = 'FritzHosts'
+
+# fc = FritzConnection(address='192.168.178.1')
+# print(fc)  # print router model informations
+
+
+fh = FritzHosts(address='192.168.178.1', user='raspi',
+                password='WwRLWlp3OcVMOna4kIIO')
+
+# d = fh.get_specific_host_entry_by_ip('192.168.178.112')
+# for key, value in d.items():
+#     print(f"{key} = {value}")
+
+hosts = fh.get_hosts_info()
+for index, host in enumerate(hosts, start=1):
+    status = 'active' if host['status'] else '-'
+    ip = host['ip'] if host['ip'] else '-'
+    mac = host['mac'] if host['mac'] else '-'
+    hn = host['name']
+    # print(f'{index:>3}: {ip:<16} {hn:<28} {mac:<17}   {status}')
+    if status == 'active':
+        print(f'{index:>3}: {ip:<16} {hn:<28} {mac:<17}   {status}')
+        d_fields = {'active': 1}
+        d_tags = {'hostname': hn}
+        influx.upload(measurement=my_measurement, fields=d_fields, tags=d_tags)
+        # print(hn)
