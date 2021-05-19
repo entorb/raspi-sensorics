e456e111f27582afe6c9a6925c8e9012fd6b38fd
Torben Menke <59419684+entorb@users.noreply.github.com>
2021-05-19 12:41:29 +0200

    improvements

--- test_mh-z19.py
+++ test_mh-z19.py
@@ -3,4 +3,5 @@
 # pip3 install mh-z19
 import mh_z19
 
-print (mh_z19.read())
+print(mh_z19.read())
+# not working because Bluetooth and serial have a conflict on Raspi3
---
+++ todo.md
@@ -0,0 +1,3 @@
+# TODOS
+* https in InfluxDB, see https://docs.influxdata.com/influxdb/v1.8/administration/https_setup/
+* https in Grafana
